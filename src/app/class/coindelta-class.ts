9d7d517cb7cb76fcd046ef8ce5dbf17633a9b450
factopic <pavankumar2r@gmail.com>
2019-01-16 08:22:07 +0530

    added Mixture Component

--- src/app/class/coindelta-class.ts
+++ src/app/class/coindelta-class.ts
@@ -6,3 +6,62 @@ export class CoindeltaClass
     Last: number;
 
 }
+
+
+/*++
+
+
+
+
+ 
+ 
+ [
+	{"Ask":264777.77,"Bid":257201.01,"MarketName":"btc-inr", 	"Last":264786.0},
+	{"Ask":8900.0,"Bid":8562.04,	 "MarketName":"eth-inr", 	"Last":8900.0},
+	{"Ask":2396.0,"Bid":2306.0,		 "MarketName":"ltc-inr", 	"Last":2399.0},
+1	{"Ask":99.5,"Bid":95.0,			 "MarketName":"omg-inr",	"Last":99.5},
+	{"Ask":183.9,"Bid":173.5,		 "MarketName":"qtum-inr",	"Last":169.99},
+	{"Ask":23.8,"Bid":23.79,		 "MarketName":"xrp-inr",	"Last":23.89},
+	
+	{"Ask":1.47,"Bid":1.44,			"MarketName":"zil-inr",				"Last":1.41},
+	{"Ask":21.76,"Bid":18.23,		"MarketName":"zrx-inr",			"Last":20.0},
+	{"Ask":10.69,"Bid":9.9,			"MarketName":"knc-inr",				"Last":11.0},
+2	{"Ask":185.0,"Bid":161.0,		"MarketName":"eos-inr",			"Last":176.5},
+	{"Ask":600.0,"Bid":530.01,		"MarketName":"neo-inr",			"Last":530.0},	
+	{"Ask":179.99,"Bid":162.0,		"MarketName":"gas-inr",			"Last":165.0},
+	
+	{"Ask":1.82,"Bid":1.76,			"MarketName":"trx-inr",				"Last":1.77},
+	{"Ask":4.8,"Bid":4.5,			"MarketName":"gnt-inr",ae			"Last":4.75},
+3	{"Ask":8.59,"Bid":7.9,			"MarketName":"bat-inr",				"Last":9.8},
+	{"Ask":3.87,"Bid":3.83,			"MarketName":"cvc-inr",				"Last":3.6},
+	{"Ask":22.0,"Bid":22.0,			"MarketName":"eng-inr",				"Last":23.5},
+	{"Ask":2.99,"Bid":2.61,			"MarketName":"mana-inr",				"Last":2.63},
+	
+	{"Ask":1.0,"Bid":0.95,			"MarketName":"spank-inr",				"Last":1.03},
+	{"Ask":17.99,"Bid":17.2,		"MarketName":"icx-inr",				"Last":20.35},
+4	{"Ask":0.69,"Bid":0.68,			"MarketName":"cnd-inr",				"Last":0.68},
+	{"Ask":10.3,"Bid":9.55,			"MarketName":"aion-inr",				"Last":9.8},
+	{"Ask":69.5,"Bid":69.0,			"MarketName":"dai-inr",				"Last":69.5},
+	{"Ask":2.8,"Bid":2.5,			"MarketName":"qkc-inr",				"Last":2.86},
+	
+	{"Ask":0.08,"Bid":0.07,			"MarketName":"krm-inr",				"Last":0.08},	
+	{"Ask":27.5,"Bid":28.0,			"MarketName":"ae-inr",				"Last":27.13},
+5	{"Ask":5.12,"Bid":4.54,			"MarketName":"ten-inr",				"Last":4.8},
+	{"Ask":75.6,"Bid":75.44,		"MarketName":"usdt-inr",			"Last":75.6},
+	{"Ask":6000.0,"Bid":1010.0,		"MarketName":"btc-usdt",				"Last":6000.0},
+	{"Ask":260.0,"Bid":61.3005,		"MarketName":"eth-usdt",				"Last":188.0},
+	
+	{"Ask":0.3999,"Bid":0.3002,		"MarketName":"xrp-usdt",				"Last":0.6584},
+	{"Ask":0.39,"Bid":0.38,			"MarketName":"spc-inr",				"Last":0.38},
+6	{"Ask":9500.0,"Bid":9000.0,		"MarketName":"bchabc-inr",				"Last":9500.0},
+	{"Ask":5994.44,"Bid":6054.0,	"MarketName":"bchsv-inr",				"Last":5705.0},
+	{"Ask":8.03,"Bid":7.9,			"MarketName":"xlm-inr",				"Last":8.16},
+	{"Ask":675.0,"Bid":73.2,		"MarketName":"pax-inr",				"Last":75.0},
+	
+	{"Ask":1.66,"Bid":1.1,			"MarketName":"lbc-inr",				"Last":1.66}
+ ]
+ 
+
+
+
+*/
