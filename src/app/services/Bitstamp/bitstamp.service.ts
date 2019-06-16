a6d0d805f8bcab7bb677f99851cacc79e6e96a03
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-06-16 23:28:05 +0530

    Update bitstamp.service.ts

--- src/app/services/Bitstamp/bitstamp.service.ts
+++ src/app/services/Bitstamp/bitstamp.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BitstampBTC } from '../../classes/Bitstamp/bitstamp-btc';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BitstampService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  bitStampUrl= 'https://koinex.in/api/ticker';
+
+  getBitstamp() : Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrl);
+  }
+
 }
