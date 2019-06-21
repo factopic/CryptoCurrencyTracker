65b81d12581cdc6c0896a42951898e99b8ed1a09
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-06-21 22:26:06 +0530

    added fetching mehod

--- src/app/services/Bitstamp/bitstamp.service.ts
+++ src/app/services/Bitstamp/bitstamp.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BitstampBTC } from '../../classes/Bitstamp/bitstamp-btc';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BitstampService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  bitStampUrlBtc= 'https://www.bitstamp.net/api/v2/ticker/btcusd/';
+  bitStampUrlEth ='https://www.bitstamp.net/api/v2/ticker/ethusd/';
+  bitStampUrlLtc ='https://www.bitstamp.net/api/v2/ticker/ltcusd/';
+  bitStampUrlXrp ='https://www.bitstamp.net/api/v2/ticker/xrpusd/';
+  bitStampUrlBch ='https://www.bitstamp.net/api/v2/ticker/bchusd/';
+  getBitstampBTC() : Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrlBtc);
+  }
+
+  getBitstampETH() : Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrlEth)
+  }
+
+  getBitStampLtc(): Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrlLtc)
+  }
+
+  getBitStampXrp(): Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrlXrp);
+  }
+
+  getBitStampBch(): Observable<BitstampBTC>
+  {
+    return this.http.get<BitstampBTC>(this.bitStampUrlBch);
+  }
 }
